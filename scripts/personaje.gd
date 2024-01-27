23e3133cc4814f792b0f18112ee980336315ce8e
Nicolas Altomonte <tenchuu@gmail.com>
2024-01-27 14:17:07 -0300

    crash

--- scripts/personaje.gd
+++ scripts/personaje.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 1000.0
 const JUMP_VELOCITY = -900.0
+const CRASH_VELOCITY = 1200
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
