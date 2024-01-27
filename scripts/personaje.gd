a336dc6e1493420406555ca3a6036eb00588af7a
Nicolas Altomonte <tenchuu@gmail.com>
2024-01-27 13:07:14 -0300

    camera

--- scripts/personaje.gd
+++ scripts/personaje.gd
@@ -26,3 +26,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().reload_current_scene()
